033bbdf20c263fb8c5dda59697ca28ff8de78a40
dujiajun <836737136@qq.com>
2020-04-21 20:13:24 +0800

    优化创世区块生成方式

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,4 +1,5 @@
 from blockchain.block import Block
+from blockchain.consensus import mine
 from blockchain.params import Params
 from blockchain.transaction import Vin, Vout, Tx
 from utils.transaction_utils import add_utxos_to_set, find_utxos_from_block
@@ -15,11 +16,13 @@ def create_genesis_block(addr, value=Params.INITIAL_MONEY) -> Block:
     tx_out = [Vout(value=value, to_addr=addr)]
 
     txs = [Tx(tx_in=tx_in, tx_out=tx_out)]
-    return Block(prev_hash=None,
-                 timestamp=12345,
-                 bits=0,
-                 nonce=0,
-                 txs=txs)
+    block = Block(prev_hash=None,
+                  timestamp=12345,
+                  bits=18,
+                  nonce=0,
+                  txs=txs)
+    nonce = mine(block)
+    return block.replace(nonce)
 
 
 def add_genesis_block(peer, genesis_block):
